a752886696b15c2b26924dc0c5f5200058100e11
LoTwT <709937065@qq.com>
2022-10-15 22:54:26 +0800

    chore: add @iconify-json/carbon icons set

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,16 @@ const Index = () => (
       <p mt-8>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
+
       <a
         class={styles.link}
         href="https://github.com/solidjs/solid"
         target="_blank"
         rel="noopener noreferrer"
+        flex
+        items-center
       >
-        Learn Solid
+        <div i-carbon-bat inline-block mr-2 /> Learn Solid
       </a>
     </header>
   </div>
