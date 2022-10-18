f3965f32ac10a82b4b9dd25aac49d50455306323
alexzhang1030 <49969959+alexzhang1030@users.noreply.github.com>
2022-10-18 08:54:10 +0800

    feat: add darkmode (#1)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,38 @@
 import logo from "@/assets/logo.svg"
 import styles from "@/styles/index.module.css"
+import { useDark } from "@colid/core"
 
-const Index = () => (
-  <div class={styles.App}>
-    <header class={styles.header}>
-      <img src={logo} class={styles.logo} alt="logo" />
-      <p mt-8>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
+const Index = () => {
+  const [dark, setDark] = useDark()
 
-      <a
-        class={styles.link}
-        href="https://github.com/solidjs/solid"
-        target="_blank"
-        rel="noopener noreferrer"
-        flex
-        items-center
-      >
-        <div i-carbon-bat inline-block mr-2 /> Learn Solid
-      </a>
-    </header>
-  </div>
-)
+  return (
+    <div class={styles.App}>
+      <header class={styles.header} dark:bg="#282c34" dark:color-white>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p mt-8>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+          flex
+          items-center
+        >
+          <div i-carbon-bat inline-block mr-2 /> Learn Solid
+        </a>
+
+        <div
+          i-carbon-moon
+          cursor-pointer
+          dark:i-carbon-sun
+          onclick={() => setDark(!dark())}
+        ></div>
+      </header>
+    </div>
+  )
+}
 
 export default Index
