cee03c4d77db208b64aac63b58f83edc86a993a1
LoTwT <709937065@qq.com>
2022-10-18 21:49:14 +0800

    chore: remove index.module.css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,40 @@
 import logo from "@/assets/logo.svg"
-import styles from "@/styles/index.module.css"
 import { useDark } from "@colid/core"
 
 const Index = () => {
   const [dark, setDark] = useDark()
 
   return (
-    <div class={styles.App}>
-      <header class={styles.header} dark:bg="#282c34" dark:color-white>
-        <img src={logo} class={styles.logo} alt="logo" />
+    <div text-center>
+      <header
+        min-h-screen
+        flex
+        flex-col
+        justify-center
+        items-center
+        text-size="[calc(10px+2vmin)]"
+        dark:bg="#282c34"
+        dark:color-white
+      >
+        <img
+          src={logo}
+          alt="logo"
+          h-40vmin
+          pointer-events-none
+          animate-spin
+          animate-duration="20s"
+        />
         <p mt-8>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
 
         <a
-          class={styles.link}
           href="https://github.com/solidjs/solid"
           target="_blank"
           rel="noopener noreferrer"
           flex
           items-center
+          color="#b318f0"
         >
           <div i-carbon-bat inline-block mr-2 /> Learn Solid
         </a>
